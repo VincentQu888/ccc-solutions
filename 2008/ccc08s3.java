6efc02f14e31bf2a1eb1f700be4d63a298144b28
VincentQu888 <112832158+VincentQu888@users.noreply.github.com>
2022-11-19 11:49:52 -0500

    Add files via upload

---
+++ 2008/ccc08s3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class ccc08s3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out = new PrintWriter(System.out);
+		
+		
+	}
+
+}
