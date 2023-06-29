315255153b5109cbc45d2cfeae01de221cac5f97
VincentQu888 <112832158+VincentQu888@users.noreply.github.com>
2023-06-29 16:24:29 -0400

    Add files via upload

---
+++ ccc09s5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class ccc09s5 {
+
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        	
+    }	
+
+}
